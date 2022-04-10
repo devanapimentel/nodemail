3515b106ce95b8e7348d389fa98cfb46588d4f6a
devanapimentel <103378207+devanapimentel@users.noreply.github.com>
2022-04-10 14:12:58 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { redirect } = require('statuses');
+const app = express()
+const port = 3000
+
+app.post('/', (req, res) => {
+    let nodemailer = require('nodemailer');
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'comunica.ana.pimentel@gmail.com',
+            pass: '@comunica2022'
+        }
+    });
+    const mailOptions = {
+        from: 'comunica.ana.pimentel@gmail.com', // sender address
+        to: 'icarobernar@hotmail.com', // receiver (use array of string for a list)
+        subject: req.body.subject, // Subject line
+        html: req.body.message// plain text body
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err)
+        res.send({message: err})
+        else
+        res.send({message: info})
+    });
+    
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
