b1b69f7bfc1079f86e582c64b39b102c71a70b91
devanapimentel <103378207+devanapimentel@users.noreply.github.com>
2022-04-10 14:22:40 -0300

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { redirect } = require('statuses');
 const app = express()
-const port = 3000
+const port = 80
 
 app.post('/', (req, res) => {
     let nodemailer = require('nodemailer');
